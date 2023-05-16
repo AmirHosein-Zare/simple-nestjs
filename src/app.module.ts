67656080cb24e0aa976f25ef968e2713402c640d
AmirHosein <awir.software@gmail.com>
2023-05-16 20:56:13 +0330

    add events module , controller , service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EventController } from './event/event.controller';
+import { EventModule } from './event/event.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [EventModule],
+  controllers: [AppController, EventController],
   providers: [AppService],
 })
 export class AppModule {}
